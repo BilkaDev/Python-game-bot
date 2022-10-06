72e2ab150335c7df1bc2d7efa52ac2b894acc3e9
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-10-06 19:39:55 +0200

    Add global path and start stop bot.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 from windowcapture import WindowCapture
 from hero import Hero
 
@@ -6,11 +9,12 @@ import pyautogui
 import pydirectinput
 import keyboard
 
+
 HEAL_SHORTCUT = '0'
 # 2 skill must be +1 key
 ATTACK_SHORTCUT = '6'
 
-CD_FIRST_ATTACK_SPELL = 4.5
+CD_FIRST_ATTACK_SPELL = 7
 HP_MIN = 0.7
 
 wincap = WindowCapture('CABAL')
@@ -21,24 +25,29 @@ pydirectinput.keyDown("z")
 time.sleep(0.2)
 pydirectinput.keyUp("z")
 
-# press "q" to stop!
-while not keyboard.is_pressed('q'):
-    # loop_time = time.time()
-    hp_target_image = wincap.get_hp_target_img()
-    # cv2.imshow('Result', fullscreen_image)
-    # cv2.waitKey()
-    isTarget = hero.is_target(hp_target_image)
-    hero.check_backpack(wincap)
-    hero.check_hp(hp_target_image, HP_MIN, HEAL_SHORTCUT)
-    if hero.is_full_backpack:
-        print("BP IS FULL")
-    # print('FPS {}'.format(time.time() - loop_time))
-    # loop_time = time.time()
+# press "q" to stop! "e"  to start
+is_run = False
+while True:
+    if is_run:
+        is_run = not keyboard.is_pressed('q')
+        # loop_time = time.time()
+        hp_target_image = wincap.get_hp_target_img()
+        # cv2.imshow('Result', fullscreen_image)
+        # cv2.waitKey()
+        isTarget = hero.is_target(hp_target_image)
+        hero.check_backpack(wincap)
+        hero.check_hp(hp_target_image, HP_MIN, HEAL_SHORTCUT)
+        if hero.is_full_backpack:
+            print("BP IS FULL")
+        # print('FPS {}'.format(time.time() - loop_time))
+        # loop_time = time.time()
 
-    if isTarget:
-        hero.attack(ATTACK_SHORTCUT)
+        if isTarget:
+            hero.attack(ATTACK_SHORTCUT)
+        else:
+            hero.stop_attack()
+            hero.get_loot()
+            hero.find_target()
+            time.sleep(0.2)
     else:
-        hero.stop_attack()
-        hero.get_loot()
-        hero.find_target()
-        time.sleep(0.2)
+        is_run = keyboard.is_pressed('e')
