5cae19c78059f32847d4a5f43ccbd268da3dc538
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-10-07 23:35:34 +0200

    test

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ while True:
         # cv2.imshow('Result', fullscreen_image)
         # cv2.waitKey()
         isTarget = hero.is_target(hp_target_image)
-        pyautogui.pixel(166,52)
-        # hero.check_hp(hp_target_image)
+        hero.check_hp(hp_target_image)
         # print('FPS {}'.format(time.time() - loop_time))
         # loop_time = time.time()
 
